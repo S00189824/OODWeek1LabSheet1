2774442df0ee5fb35d423895837b54d8ce35a3ef
hypensanez12 <46888854+hypensanez12@users.noreply.github.com>
2020-01-21 15:41:33 +0000

    Lab1

---
+++ OODWeek1Lab1/OODWeek1Lab1/Band.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OODWeek1Lab1
+{
+    class Band
+    {
+        public string BandName { get; set; }
+        public DateTime YearFormed { get; set; }
+        
+
+
+    }
+}
