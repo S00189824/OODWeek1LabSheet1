c8dc19387482ec5fd45314d48a1df621dc2393d1
David Coleman <46888854+S00189824@users.noreply.github.com>
2020-01-22 12:51:32 +0000

    Icomparable finished

--- OODWeek1Lab1/OODWeek1Lab1/Band.cs
+++ OODWeek1Lab1/OODWeek1Lab1/Band.cs
@@ -6,13 +6,29 @@ using System.Threading.Tasks;
 
 namespace OODWeek1Lab1
 {
-    class Band
+    public class Band : IComparable
     {
         public string BandName { get; set; }
         public DateTime YearFormed { get; set; }
 
-        public string[] Members = new string[] { "Tom Smith", "Brad Duggan", "Micheal Jonrs" };
+        public List<string> Members { get; set; }
 
+        public Band(string bandname,DateTime year,List<string> members)
+        {
+            BandName = bandname;
+            YearFormed = year;
+            Members = members;
+        }
 
+        public override string ToString()
+        {
+            return BandName;
+        }
+
+        public int CompareTo(object obj)
+        {
+            Band that = (Band)obj;
+            return this.BandName.CompareTo(that.BandName);
+        }
     }
 }
--- OODWeek1Lab1/OODWeek1Lab1/MainWindow.xaml.cs
+++ OODWeek1Lab1/OODWeek1Lab1/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,7 +20,8 @@ namespace OODWeek1Lab1
 
     public partial class MainWindow : Window
     {
-        List<Band> bands = new List<Band>();
+        Band[] bands = new Band[6];
+        ObservableCollection<Band> band;
 
         public MainWindow()
         {
@@ -28,7 +30,38 @@ namespace OODWeek1Lab1
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
+            BandCreate();
 
+        }
+
+        void BandCreate()
+        {
+
+            List<string> n1 = new List<string> { "name1", "Name2", "Name3" };
+            List<string> n2 = new List<string> { "name4", "Name5" };
+            List<string> n3 = new List<string> { "name6", "Name7" };
+            List<string> n4 = new List<string> { "name8", "Name9" };
+            List<string> n5 = new List<string> { "name10", "Name11" };
+            List<string> n6 = new List<string> { "name12", "Name13" };
+
+            Band b1 = new Band("Band1", new DateTime(2019, 09, 10), n1);
+            Band b2 = new Band("Band2", new DateTime(2018, 09, 10), n2);
+            Band b3 = new Band("Band3", new DateTime(2017, 09, 10), n3);
+            Band b4 = new Band("Band4", new DateTime(2016, 09, 10), n4);
+            Band b5 = new Band("Band5", new DateTime(2014, 09, 10), n5);
+            Band b6 = new Band("Band6", new DateTime(2015, 09, 10),n6);
+
+            List<Band> bands = new List<Band>();
+            bands.Add(b1);
+            bands.Add(b2);
+            bands.Add(b3);
+            bands.Add(b4);
+            bands.Add(b5);
+            bands.Add(b6);
+
+            LbxBands.ItemsSource = bands;
+
+            bands.Sort()
         }
     }
 }
